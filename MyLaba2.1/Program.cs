1f996112bb487825c9281f6995a5a2826cbda5a6
dinikul <pachekhom.d@gmail.com>
2020-04-01 14:21:30 +0300

    laba2

---
+++ MyLaba2.1/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace MyLaba2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Piano piano = new Piano();
+            piano.Customize_the_Piano();
+            piano.Play_the_Piano();
+        }
+    }
+    class Piano
+    {
+        public Button button;
+        public Pedal pedal;
+        public Piano()
+        {
+            button = new Button();
+            pedal = new Pedal();
+        }
+        public void Customize_the_Piano()
+        {
+            Console.WriteLine("customize the piano");
+            var customize = Console.ReadLine();
+            if (customize == "customize")
+            {
+                Console.WriteLine("The piano is tuned");
+            }
+            else
+            {
+                Console.WriteLine("The piano is not configured");
+            }
+        }
+        public void Play_the_Piano()
+        {
+            while (button.status)
+            {
+                if (Console.KeyAvailable == true)
+                {
+                    var keyInfo = Console.ReadKey();
+                    button.Pressing_a_Key(keyInfo);
+                    pedal.Pressing_the_Pedal(keyInfo);
+                }
+                
+            }
+        }
+    }
+    class Button
+    {
+        public bool status = true;
+        public SoundPlayer DO;
+        public SoundPlayer RE;
+        public SoundPlayer MI;
+        public SoundPlayer FA;
+        public SoundPlayer SOL;
+        public SoundPlayer LIA;
+        public SoundPlayer SI;
+        public void Pressing_a_Key(ConsoleKeyInfo keyInfo)
+        {
+          
+            if (keyInfo.Key == ConsoleKey.Q)
+            {
+                
+                DO = new SoundPlayer(@"C:\Users\Home\Downloads\noty-do.wav");
+                DO.Play();
+                Console.Clear();
+                
+            }
+            if (keyInfo.Key == ConsoleKey.W)
+            {
+                RE = new SoundPlayer(@"C:\Users\Home\Downloads\re.wav");
+                RE.Play();
+                Console.Clear();
+            }
+            if (keyInfo.Key == ConsoleKey.E)
+            {
+                MI = new SoundPlayer(@"C:\Users\Home\Downloads\mi (online-audio-converter.com).wav");
+                MI.Play();
+                Console.Clear();
+            }
+            if (keyInfo.Key == ConsoleKey.R)
+            {
+                FA = new SoundPlayer(@"C:\Users\Home\Downloads\fa (online-audio-converter.com).wav");
+                FA.Play();
+                Console.Clear();
+            }
+            if (keyInfo.Key == ConsoleKey.T)
+            {
+                SOL = new SoundPlayer(@"C:\Users\Home\Downloads\sol (online-audio-converter.com).wav");
+                SOL.Play();
+                Console.Clear();
+            }
+            if (keyInfo.Key == ConsoleKey.Y)
+            {
+                LIA = new SoundPlayer(@"C:\Users\Home\Downloads\lja (online-audio-converter.com).wav");
+                LIA.Play();
+                Console.Clear();
+            }
+            if (keyInfo.Key == ConsoleKey.U)
+            {
+                SI = new SoundPlayer(@"C:\Users\Home\Downloads\si (online-audio-converter.com).wav");
+                SI.Play();
+                Console.Clear();
+            }
+            if (keyInfo.Key == ConsoleKey.I)
+            {
+                status = false;
+            }
+        }
+    }
+    class Pedal
+    {
+        public string right_pedal = "пiдняття всiх демпфер";
+        public string left_pedal = "послаблення звуку";
+        public string middle_pedal = "затримка обраних демпферiв у пiднятому станi";
+        public void Pressing_the_Pedal(ConsoleKeyInfo keyInfo)
+        {
+
+            if (keyInfo.Key == ConsoleKey.G)
+            {
+                Console.WriteLine(right_pedal);
+            }
+
+            if (keyInfo.Key == ConsoleKey.F)
+            {
+                Console.WriteLine(middle_pedal);
+            }
+
+            if (keyInfo.Key == ConsoleKey.D)
+            {
+                Console.WriteLine(left_pedal);
+            }
+        }
+    }
+}
---
+++ MyLaba2.2/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLaba2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double interest_basic_job = 0.2;
+            double interest_extra_job = 0.15;
+            double interest_royalties = 0.1;
+            double interest_sale_of_property = 0.05;
+            double interest_transfers_from_abroad = 0.05;
+
+            Podatok os_robota = new Podatok("Oсновна робота", interest_basic_job * 20000);
+            Podatok extra_job = new Podatok("Додаткова робота", interest_extra_job * 10000);
+            Podatok royalties = new Podatok("royalties", interest_royalties * 30000);
+            Podatok sale_of_property = new Podatok("sale_of_property", interest_sale_of_property * 40000);
+            Podatok transfers_from_abroad = new Podatok("transfers_from_abroad", interest_transfers_from_abroad * 40000);
+            Podatki podatki = new Podatki(os_robota, extra_job, royalties, sale_of_property, transfers_from_abroad);
+            podatki.ShowList();
+            podatki.SortPodatok();
+            Console.WriteLine("");
+            Console.WriteLine("");
+            podatki.ShowList();
+            podatki.SumaPodatkiv();
+        }
+    }
+    class Podatok
+    {
+       
+        public string name;
+        public double podatok;
+        public Podatok(string Name, double Podatok)
+        {
+            name = Name;
+            podatok = Podatok;
+        }
+
+    }
+    class Podatki
+    {
+        public List<Podatok> ListPodatkiv = new List<Podatok>();
+        public Podatki(params Podatok[] p)
+        {
+            foreach (var i in p)
+            {
+                AddPodatok(i);
+            }
+        }
+        public void AddPodatok(Podatok podatok)
+        {
+            ListPodatkiv.Add(podatok);
+        }
+        public void ShowList()
+        {
+            Console.WriteLine($"{"Name",10}{"Suma Podatka", 10}");
+            foreach(var i in ListPodatkiv)
+            {
+                Console.WriteLine($"{i.name,10}{i.podatok, 10}");
+            }
+        }
+        public void SortPodatok()
+        {
+            for(int i = 0; i < ListPodatkiv.Count; i++)
+            {
+                for(int j = i + 1; j < ListPodatkiv.Count; j++)
+                {
+                    if (ListPodatkiv[i].podatok > ListPodatkiv[j].podatok)
+                    {
+                        var sort = ListPodatkiv[i];
+                        ListPodatkiv[i] = ListPodatkiv[j];
+                        ListPodatkiv[j] = sort;
+                    }
+                }
+            }
+        }
+        public void SumaPodatkiv()
+        {
+            double suma = 0;
+            foreach (var n in ListPodatkiv)
+            {
+                suma += n.podatok;
+            }
+            Console.WriteLine("");
+            Console.WriteLine(suma);
+        }
+    }
+}
